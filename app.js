2d2c5f1c068813fb0ed5360971a4f20883084a69
Carla Vidales <carlavidales.ca@gmail.com>
2023-11-17 17:10:48 -0800

    Finalize project

--- app.js
+++ app.js
@@ -3,21 +3,8 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const { sequelize } = require("./models/index");
-
-// test connection to the database & sync the model
-// (async () => {
-//   await sequelize.sync();
-//   try {
-//     await sequelize.authenticate();
-//     console.log("Connection to the database successful!");
-//   } catch (error) {
-//     console.log("Error connecting to the database: ", error);
-//   }
-// })();
 
 const indexRouter = require("./routes/index");
-// const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -32,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-// app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
